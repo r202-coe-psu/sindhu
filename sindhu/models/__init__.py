774554335f4f70973ae11bdbf3f1a44b43242091
Soravit <soravit.sukkarn@gmail.com>
2026-04-28 21:16:29 +0700

    fix: sindhu pydantic settings

---
+++ sindhu/models/__init__.py
@@ -0,0 +1,67 @@
+# import motor.motor_asyncio
+import sys
+from typing import Sequence, Type, TypeVar
+from inspect import getmembers, isclass
+import beanie
+import pymongo
+from pydantic_settings import BaseSettings
+
+
+DocumentType = TypeVar("DocumentType", bound=beanie.Document)
+
+
+class AppSettings(BaseSettings):
+    MONGODB_URI: str = "mongodb://host.docker.internal:27017/dharadb"
+
+    class Config:
+        env_file = ".env"
+        extra = "allow"
+
+
+async def gather_documents() -> Sequence[Type[DocumentType]]:
+    """Returns a list of all MongoDB document models defined in `models` module."""
+
+    class_models = getmembers(sys.modules[__name__], isclass)
+
+    for key in [k for k in sys.modules if __name__ in k]:
+        class_models.extend(getmembers(sys.modules[key], isclass))
+
+    class_models = list(set(class_models))
+
+    return [
+        doc
+        for _, doc in class_models
+        if issubclass(doc, beanie.Document) and doc.__name__ != "Document"
+    ]
+
+
+class BeanieClient:
+    async def init_beanie(self, settings):
+        self.settings = settings
+        self.client = pymongo.AsyncMongoClient(settings.MONGODB_URI)
+        self.db = self.client.get_default_database()
+
+        documents = await gather_documents()
+
+        print("Documents >>>")
+        for document in documents:
+            print(document)
+
+        await beanie.init_beanie(
+            database=self.db,
+            document_models=documents,
+            # recreate_views=True,
+        )
+
+
+async def init_beanie(app, settings):
+    await beanie_client.init_beanie(settings)
+
+
+async def init_default_beanie_client():
+    settings = AppSettings()
+    print("setings>>>", settings)
+    await beanie_client.init_beanie(settings)
+
+
+beanie_client = BeanieClient()
